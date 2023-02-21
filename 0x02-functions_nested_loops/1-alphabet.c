c79a0e97a8eb132e0d4fd7af70d0c67c9b8c8331
tunjiy <tunjiy95@gmail.com>
2023-02-21 11:35:11 -0800

    print lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+
+	char r;
+
+	for (r = 'a'; r <= 'z'; r++)
+	{
+	_putchar(r);
+	}
+	_putchar('\n');
+}
+
